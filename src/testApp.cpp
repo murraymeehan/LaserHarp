8fac58074e432590cd567a66f411aa4703a24205
Murray Meehan <murray.meehan@gmail.com>
2012-03-13 12:50:25 -0700

    Reintegrating latest build with master branch, commit 2 of 2

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,50 +1,13 @@
 #include "testApp.h"
 
-<<<<<<< HEAD
 //--------------------------------------------------------------
 void testApp::setup() {
 	detector.setup();
 	generator.setup();
-=======
-using namespace std;
-
-void testApp::setup() {
-
-	isRecording		= false;
-
-	nearThreshold = 0;
-	farThreshold  = 5000;
-
-	setupRecording();
-	ofBackground(0, 0, 0);
-
-}
-
-void testApp::setupRecording(string _filename) {
-
-#ifdef USE_XML_NODE_CONFIG
-	recordContext.setupUsingXMLFile();
-#else
-	recordContext.setup();	// all nodes created by code -> NOT using the xml config file at all
-#endif
-	recordDepth.setup(&recordContext);
-	recordImage.setup(&recordContext);
-
-	recordUser.setup(&recordContext);
-	recordUser.setMaxNumberOfUsers(2);// use this to set dynamic max number of users (NB: that a hard upper limit is defined by MAX_NUMBER_USERS in ofxUserGenerator)
-
-	recordHandTracker.setup(&recordContext, 4);
-
-	recordContext.toggleRegisterViewport();
-
-	oniRecorder.setup(&recordContext, ONI_STREAMING);
-
->>>>>>> 85b2243ab5e2810d60832902e1111ea9556d89f9
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-<<<<<<< HEAD
 	detector.update();
 	generator.update();
 	
@@ -62,140 +25,5 @@ void testApp::draw(){
 void testApp::keyPressed  (int key){
 	detector.keyPressed(key);
 	generator.play(detector.pluckParams);
-=======
-
-    // update all nodes
-    recordContext.update();
-    recordDepth.update();
-    recordImage.update();
-
-    // demo getting depth pixels directly from depth gen
-    depthRangeMask.setFromPixels(recordDepth.getDepthPixels(nearThreshold, farThreshold),
-                                 recordDepth.getWidth(), recordDepth.getHeight(), OF_IMAGE_GRAYSCALE);
-
-    // update tracking/recording nodes
-    recordUser.update();
-    if (isRecording)
-        oniRecorder.update();
-
-}
-
-//--------------------------------------------------------------
-void testApp::detectStrum(){
-
-    if (recordHandTracker.getNumTrackedHands() > 0) {
-
-        // if there are 1 or more hands detected, this is the first hand
-        ofxTrackedHand MurrayHand = *recordHandTracker.getHand(0);
-
-        float x = MurrayHand.projectPos.x;
-        float y = MurrayHand.projectPos.y;
-        float z = MurrayHand.projectPos.z;
-        int t = ofGetElapsedTimeMillis();
-
-        printf(" %f , %f , %f , %i \n,",x,y,z,t);
-
-//        string data2 = " %f , %f , %f , %i \n,"%(x,y,z,t);
-//        stringstream data;
-//        data << x << " , " << y << " , " << z << " , " << t << " \n";
-        if (isRecording) {
-//            Write values to text file
-//            logFile << data;
-            logFile << x << " , " << y << " , " << z << " , " << t << " \n";
-        }
-
-//        string _timestamp = ofToString(ofGetDay()) +
-//	ofToString(ofGetMonth()) +
-//	ofToString(ofGetYear()) +
-//	ofToString(ofGetHours()) +
-//	ofToString(ofGetMinutes()) +
-//	ofToString(ofGetSeconds());
-    }
-}
-
-//--------------------------------------------------------------
-void testApp::draw(){
-
-	ofSetColor(255, 255, 255);
-	glPushMatrix();
-	glScalef(0.75, 0.75, 0.75);
-
-    recordDepth.draw(0,0,640,480);
-    recordImage.draw(640, 0, 640, 480);
-
-    depthRangeMask.draw(0, 480, 320, 240);	// can use this with openCV to make masks, find contours etc when not dealing with openNI 'User' like objects
-
-//    recordUser.draw(); // uncomment this to draw a silly stickman skeleton
-    recordHandTracker.drawHands();
-    detectStrum();
-
-	glPopMatrix();
-	ofSetColor(255, 255, 0);
-
-	string statusRec		= (string)(!isRecording ? "READY" : "RECORDING");
-	stringstream msg;
-
-	msg
-	<< "    s : start/stop recording  : " << statusRec << endl
-	<< endl
-	<< "File  : " << oniRecorder.getCurrentFileName() << endl;
-
-	ofDrawBitmapString(msg.str(), 20, 560);
-
-}
-
-
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-
-	switch (key) {
-		case 's':
-		case 'S':
-			if (isRecording) {
-				oniRecorder.stopRecord();
-				isRecording = false;
-				logFile.close();
-				break;
-			} else {
-				oniRecorder.startRecord(generateFileName());
-				isRecording = true;
-				//	logFile.open(generateFileName(".txt"));
-                logFile.open("log.txt");
-				break;
-			}
-			break;
-		case 'q':
-		default:
-            if (isRecording) {
-				oniRecorder.stopRecord();
-            }
-            logFile.close();
-			break;
-	}
-}
-
-
-string testApp::generateFileName(string suffix) {
-
-	string _root = "kinectRecord";
-
-	string _timestamp = ofToString(ofGetDay()) +
-	ofToString(ofGetMonth()) +
-	ofToString(ofGetYear()) +
-	ofToString(ofGetHours()) +
-	ofToString(ofGetMinutes()) +
-	ofToString(ofGetSeconds());
-
-//	string _filename = (_root + _timestamp + ".oni");
-	string _filename = (_root + _timestamp + suffix);
-
-	return _filename;
-
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
->>>>>>> 85b2243ab5e2810d60832902e1111ea9556d89f9
 }
 
--- src/testApp.h
+++ src/testApp.h
@@ -1,28 +1,5 @@
-<<<<<<< HEAD
 #ifndef _TEST_APP
 #define _TEST_APP
-=======
-/* Changelog:
-20120123:
-1. Find way to retrieve hand position
-
-*/
-
-# pragma once
-
-#ifndef _TEST_APP
-#define _TEST_APP
-#if defined (TARGET_OSX) //|| defined(TARGET_LINUX) // only working on Mac/Linux at the moment (but on Linux you need to run as sudo...)
-    // || defined (TARGET_LINUX) // linux hardware controls via libUSB aren't working in this distro of ofxOpenNI.
-    #define HARDWARE_CONTROLS
-#endif
-//#define USE_XML_NODE_CONFIG // Uncomment this to use an XML config file to select Kinect's OpenNI node. Otherwise it is hardcoded.
-//#define USE_IR // Uncomment this to use infra red instead of RGB cam...
-
-#include "ofxOpenNI.h"
-#include "ofxMarsyasNetwork.h"
-#include "ofMain.h"
->>>>>>> 85b2243ab5e2810d60832902e1111ea9556d89f9
 
 #ifndef _PLUCK_PARAMS
 	#define _PLUCK_PARAMS
@@ -40,61 +17,13 @@
 class testApp : public ofBaseApp{
 
 public:
-
-
 	void setup();
 	void update();
 	void draw();
 	void keyPressed  (int key);
-<<<<<<< HEAD
 	
 	pluckDetector detector;
 	pluckGenerator generator;
-=======
-	void windowResized(int w, int h);
-
-	void	setupRecording(string _filename = "");
-//	void	setupPlayback(string _filename);
-	string	generateFileName(string ending = ".oni");
-
-	bool				isLive, isTracking, isRecording, isCloud, isCPBkgnd, isMasking;
-	bool				isTrackingHands, isFiltering;
-
-	ofxOpenNIContext	recordContext, playContext;
-	ofxDepthGenerator	recordDepth, playDepth;
-
-#ifdef USE_IR
-	ofxIRGenerator		recordImage, playImage;
-#else
-	ofxImageGenerator	recordImage, playImage;
-#endif
-
-	ofxHandGenerator	recordHandTracker, playHandTracker;
-
-	ofxUserGenerator	recordUser, playUser;
-	ofxOpenNIRecorder	oniRecorder;
-
-//	void				drawMasks();
-	void				drawPointCloud(ofxUserGenerator * user_generator, int userID);
-
-	int					nearThreshold, farThreshold;
-//	int					pointCloudRotationY;
-
-	ofImage				allUserMasks, user1Mask, user2Mask, depthRangeMask;
-
-//	float				filterFactor;
-
-	void                detectStrum();
-//	void                mwmPlayNote();
-//    void                sfplay3(string sfName);
-//    float               distance3D(float x1, float y1, float z1, float x2, float y2, float z2);
-//    ofxMarsyasNetwork   mgn;
-
-//    ofSoundPlayer  beats;
-//    ofTrueTypeFont	font;
-
-    ofstream logFile;
->>>>>>> 85b2243ab5e2810d60832902e1111ea9556d89f9
 };
 
 #endif
